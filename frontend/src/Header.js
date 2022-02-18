c8cda2e80588e95a8be08380eba064b0a335367f
fullbread9977 <96777942+fullbread9977@users.noreply.github.com>
2022-02-18 10:46:52 +0900

    Update Header.js

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -6,9 +6,9 @@ const Header = () =>(
     <header>
     <div class="top_menu">
   <ul>
-      <li><a href="/Main2">메인으로</a></li>
+      <li><a href="/Main">정책 페이지</a></li> 
       <li> |</li>
-      <li><a href="/Main">정책 페이지</a></li>   
+         
   </ul>
 </div>
 </header>
